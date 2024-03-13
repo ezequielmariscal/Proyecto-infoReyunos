d03c39c64ca839f1a97593417d496ac2bfad7af5
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2024-03-12 23:54:52 -0300

    Add_pequeños_cambios_al_header

--- frontend/infoReyunos/src/App.jsx
+++ frontend/infoReyunos/src/App.jsx
@@ -1,12 +1,10 @@
 
+import Header from "./components/Header/Header"
 function App() {
   
   return (
     <>
-      <div className="App">
-        <h1>Hola zequi</h1>
-      </div>
-      
+      <Header/>
     </>
   )
 }
