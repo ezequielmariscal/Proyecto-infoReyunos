47df753ae35943c72e518d0c92a41c9a15aed669
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2024-03-13 22:36:07 -0300

    Add_tailwind_and_components

--- frontend/infoReyunos/src/App.jsx
+++ frontend/infoReyunos/src/App.jsx
@@ -1,10 +1,12 @@
 
-import Header from "./components/Header/Header"
+import Home from './pages/Home'
+import './App.css'
+
 function App() {
   
   return (
     <>
-      <Header/>
+      <Home/>
     </>
   )
 }
