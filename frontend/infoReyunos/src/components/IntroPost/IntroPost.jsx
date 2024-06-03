ddb2c7841fa7ab8d4d85a17b507640bab7c54158
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2024-06-03 20:13:42 -0300

    Add_p1

--- frontend/infoReyunos/src/components/IntroPost/IntroPost.jsx
+++ frontend/infoReyunos/src/components/IntroPost/IntroPost.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-const introReyu = <p>Bienvenido a la zona de reyunos este es un blog donde se explican sus caracteristicas</p>
+const introReyu = <h2>Bienvenido a la zona de reyunos este es un blog donde se explican sus caracteristicas</h2>
+
 
 function IntroPost() {
 
@@ -9,6 +10,23 @@ function IntroPost() {
     <div>
       <h1>Introducción Embalse los Reyunos</h1>
       {introReyu}
+      <h3>El presente trabajo se ha realizadoo para aportar conocimiento acerca de 
+        la cuenca el diamante y principalmente sobre "Los Reyunos", surge a travéz
+        de exposiciones verbales implementada en la zona turistica para socabar dudas
+        que puedan surgir.
+        <br></br>
+        Dicho lugar se encuentra ubicado al sur de la provincia de Mendoza y su conoci
+        miento es de gran importancia para la puesta de valor tanto natural como cultural.
+        <br></br>
+        En ésta pagina se busca contribuir sobre el cuidado y toma ded conciencia sobre la
+        biodiversidad de flora y fauna de la provincia.
+      </h3>
+        <article>
+          <div><img></img></div>
+            <div>
+
+            </div>
+        </article>
 
     </div>
   )
